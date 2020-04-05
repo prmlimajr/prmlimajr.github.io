efb83f9dee371ab1b7827e6e081c8989d21341a2
Paulo Lima <47723610+prmlimajr@users.noreply.github.com>
2020-04-04 23:12:50 -0300

    Add files via upload

---
+++ oquetemotiva/js/app.js
@@ -0,0 +1,89 @@
+(function (){
+
+    'use strict'
+
+    const time = document.querySelector('#time');
+    const greeting = document.querySelector('#greeting');
+    const name = document.querySelector('#name');
+    const focus = document.querySelector('#focus');
+
+    function showTime(){
+        let today = new Date();
+        let hour = today.getHours();
+        let minute = today.getMinutes();
+        let second = today.getSeconds();
+        time.innerHTML = `${addZero(hour)} : ${addZero(minute)} : ${addZero(second)}`;
+        setTimeout(showTime, 1000);
+    };
+
+    function addZero(time){
+        return (Number(time) < 10 ? '0' : '') + time;
+    };
+
+    function setBgGreet(){
+        let today = new Date();
+        let hour = today.getHours();
+
+        if(hour >= 5 && hour < 12){
+            document.body.style.backgroundImage = "url('css/img/ceu-azul.jpg')";
+            greeting.textContent = 'Bom Dia, ';
+        } else if (hour >= 12 && hour < 18) {
+            document.body.style.backgroundImage = "url('css/img/entardecer.jpg')";
+            greeting.textContent = 'Boa Tarde, ';
+        } else {
+            document.body.style.backgroundImage = "url('css/img/noite.jpeg')";
+            greeting.textContent = 'Boa Noite, ';
+            document.body.style.color = 'white';
+        };
+    };
+
+    function getName(){
+        if(localStorage.getItem('name') === null){
+            name.textContent = '[Digite seu nome]';
+        } else {
+            name.textContent = localStorage.getItem('name');
+        }
+    };
+
+    function setName(e){
+        if(e.type === 'keypress') {
+            if (e.which == 13 || e.keyCode == 13) {
+                localStorage.setItem('name', e.target.innerText);
+                name.blur();
+            }
+        } else {
+            localStorage.setItem('name', e.target.innerText);
+        }
+    };
+
+    function getFocus(){
+        if(localStorage.getItem('focus') === null){
+            focus.textContent = '[Digite seu objetivo]';
+        } else {
+            focus.textContent = localStorage.getItem('focus');
+        }
+    };
+
+    function setFocus(e){
+        if(e.type === 'keypress') {
+            if (e.which == 13 || e.keyCode == 13) {
+                localStorage.setItem('focus', e.target.innerText);
+                focus.blur();
+            }
+        } else {
+            localStorage.setItem('focus', e.target.innerText);
+        }
+    };
+     
+    name.addEventListener('keypress', setName);
+    name.addEventListener('blur', setName);
+    focus.addEventListener('keypress', setFocus);
+    focus.addEventListener('blur', setFocus);
+
+    showTime();
+    setBgGreet();
+    getName();
+    getFocus();
+
+})();
+
